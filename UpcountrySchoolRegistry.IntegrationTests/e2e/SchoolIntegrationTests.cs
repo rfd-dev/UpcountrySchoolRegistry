49704f56a225e6407871f269d72c9594823d18e7
RAFAEL F DALMEIDA <rafael.almeida@multicomnet.com.br>
2020-09-21 02:13:56 -0300

    + integration tests

---
+++ UpcountrySchoolRegistry.IntegrationTests/e2e/SchoolIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UpcountrySchoolRegistry.API;
+using Xunit;
+
+namespace UpcountrySchoolRegistry.IntegrationTests.e2e
+{
+    public class SchoolIntegrationTests : IClassFixture<WebApplicationFactory<UpcountrySchoolRegistry.API.Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        #region Constructor
+        public SchoolIntegrationTests(WebApplicationFactory<UpcountrySchoolRegistry.API.Startup> factory)
+        {
+            this._factory = factory;
+        }
+        #endregion
+
+        [Fact]
+        public async Task Get_InformaID_RecebeEntidade()
+        {
+            // Arrange
+            var client = this._factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(@"/school");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
